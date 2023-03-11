a09e9cba39c27cd5a819017b7e24bd1353326848
sabrinaefsilva <sabrina.e.franca@gmail.com>
2023-03-11 18:33:46 -0300

    alterações nas class

---
+++ emailESenha-prova/src/test/java/emailESenhaTest.java
@@ -0,0 +1,23 @@
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class emailESenhaTest {
+
+	@Test
+	public void testEmail() {
+
+		String email = ("Sabrina");
+		assertTrue(email, true);
+
+	}
+	
+	@Test
+	public void testSenha() {
+
+		String senha = ("123456789-10");
+		assertTrue(senha, true);
+	}
+
+}
